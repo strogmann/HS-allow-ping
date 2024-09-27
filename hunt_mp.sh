9a4c19de6f28ec76e78d256c3d4166b64bd17f9b
strogmann <nicholas.martin.galisteo@gmail.com>
2024-09-27 14:51:03 -0400

    Fixed hunt_mp.sh not closing when enter is pressed.

--- hunt_mp.sh
+++ hunt_mp.sh
@@ -2,32 +2,32 @@
 
 echo "The script will now enable EAC ipv4 ping for current session" && sleep 2
 echo
+
 gid=$(id -g)
 sudo sysctl -w net.ipv4.ping_group_range="0 $gid"
 echo
+
 echo "EAC ping is enabled for this session" && sleep 2
 echo
-read -p "Do you wish to enable ipv4 ping for your user account by default? (y/n) " yn
 
-while true; do
-    case $yn in
+read -p "Do you wish to enable ipv4 ping for your user account by default? (y/n) " yn  
 
-	[Yy]* )
-		ping_allow_write=net.ipv4.ping_group_range="0 $gid";
-		echo "$ping_allow_write;" | sudo tee /etc/sysctl.d/local.conf;
-		echo "The process is finished. \n" && sleep 1;		read -rsn1 -p"Press enter to close";
-		kill $PPID;;
 
-	[Nn]* )
-                echo "The process is finished. \n"
-		read -rsn1 -p"Press enter to close";
-		kill $PPID;;
+case $yn in
+    [Yy]* )
+        ping_allow_write=net.ipv4.ping_group_range="0 $gid"
+        echo "$ping_allow_write;" | sudo tee /etc/sysctl.d/local.conf
+        echo "The process is finished. \n"
+        read -rsn1 -p"Press enter to close"
+        ;;
+    [Nn]* )
+        echo "The process is finished."
+        read -rsn1 -p"Press enter to close"
+        ;;
+    * )
+        echo "Please answer yes or no."
+        echo "invalid response - please execute the script again"
+        read -rsn1 -p"Press enter to close"
+        ;;
+esac
 
-# all other answers
-	* )
-		echo "Please answer yes or no.";
-		echo "invalid response - please execute the script again \n";
-		read -rsn1 -p"Press enter to close";
-		kill $PPID;;
-    esac
-done
